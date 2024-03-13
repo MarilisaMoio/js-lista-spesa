629db9350cae8a6c96ed0970c15741ecb27390c8
Marilisa Moio <moiomarilisa@gmail.com>
2024-03-13 15:29:44 +0100

    feat: add ver-1

--- ver-1/js/script.js
+++ ver-1/js/script.js
@@ -1,1 +1,20 @@
 //versione 1: dato l'array della spesa, stampare singolarmente gli elementi nel DOM con il FOR 
+
+//lista di partenza
+const list = [
+    'Latte',
+    'Cacao',
+    'Insalata',
+    'Carote',
+    'Pomodori',
+    'Piatti pronti'
+];
+
+//cattura dell'ul
+const groceryContainer = document.querySelector(".groceries");
+
+//stamp per ogni elemento
+for (let i = 0; i < list.length; i++){
+    const thisItem = list[i];
+    groceryContainer.innerHTML += `<li>${thisItem}</li>`;
+}
