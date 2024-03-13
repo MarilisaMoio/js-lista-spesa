101085e0a5a96b9722abe5b9517f18c9ca01d7e7
Marilisa Moio <moiomarilisa@gmail.com>
2024-03-13 15:39:41 +0100

    feat: add ver-2

--- ver-2/js/script.js
+++ ver-2/js/script.js
@@ -1,1 +1,23 @@
 //versione 2: dato l'array della spesa, stampare singolarmente gli elementi nel DOM con il WHILE
+
+//lista di partenza
+const list = [
+    'Latte',
+    'Cacao',
+    'Insalata',
+    'Carote',
+    'Pomodori',
+    'Piatti pronti'
+];
+
+//richiamo all'ul
+const groceryContainer = document.querySelector(".groceries");
+
+//counter e relativo ciclo while per l'inserimento nei li e stamp
+let counter = 0;
+
+while(counter < list.length){
+    const thisItem = list[counter];
+    groceryContainer.innerHTML += `<li>${thisItem}</li>`;
+    counter++;
+}
